d8ae2053705863924c919b9d6ffb487821f9badd
kaciel13 <kaciel13@yahoo.com>
2025-09-21 19:53:17 +0600

    v0.1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Security;
+using System.Threading.Tasks;
+
+
+class Client
+{
+    const string chatCode = "chat";
+    //const string api = "https://testserver-kaciel13.amvera.io/"; // для публкации
+    const string api = "http:localhost:5000/"; // для разработки;
+
+    static async Task Main()
+    {
+        Console.Write("name?: ");
+        var name = Console.ReadLine()?.Trim();
+
+        if (string.IsNullOrEmpty(name)) name = "Nothing";
+
+
+
+
+
+
+    }
+
+    
+
+
+
+
+}
